5f34b2a37d440e4ba97b077a641047b70b1cb1d7
Josh Cheung <joshuawcheung@ucla.edu>
2017-05-05 22:04:57 -0700

    Started encoding feature vectors

--- your_script.R
+++ your_script.R
@@ -1,1 +1,18 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite()
+library(DNAshapeR)
+library(caret)
 
+path <- "D:/GitHub/BISC577/gcPBM/"
+fa.files <- dir(path, pattern="\\.txt.fa$")
+txt.files <- dir(path, pattern="\\.txt$")
+
+## Predict DNA shapes
+fn_fasta <- "D:/GitHub/BISC577/gcPBM/Mad.txt.fa"
+pred <- getShape(fn_fasta)
+
+## Encode feature vectors
+##M
+featureType <- c("1-mer", "1-shape")
+featureVector <- encodeSeqShape(fn_fasta, pred, featureType)
+head(featureVector)
